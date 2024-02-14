1c08b9e0587ec550f3e5259a62c225562bdea0eb
Hariom Singh <157329018+hariomhks080@users.noreply.github.com>
2024-02-14 02:27:12 +0000

    deploy

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,11 +4,11 @@ import classes from './Cart.module.css';
 const Cart = (props) => {
   
   return (
-    <div >
+  
         
         <button className={classes.button} onClick={props.shown}>Cart</button>
         
-    </div>
+    
   )
 }
 
